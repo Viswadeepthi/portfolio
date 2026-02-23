a64a02e5e756e8b1b48fbf1c9909f90eba9af864
Viswadeepthi <deepthithangaraj814@gmail.com>
2026-02-23 22:06:41 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-console.log("JS connected");
+const sections = document.querySelectorAll(".section");
+
+const revealOnScroll = () => {
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    const triggerPoint = window.innerHeight - 120;
+
+    if (top < triggerPoint) {
+      section.classList.add("show");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
